292fc00ef2197356863c007e28dcc73dd1d26bf8
ipc103 <iancandysemail@gmail.com>
2017-05-02 12:07:24 -0400

    remove comments

--- index.js
+++ index.js
@@ -1,56 +1,23 @@
 $(document).ready(function(){
-  console.log('Document ready!')
-
   // Selects Different HTML Elements
   const $input = $('input#query')
   const $bookList = $('ul#books')
   const $form = $('form#book-search')
 
+  // on submit, fires a function
+  $form.on('submit', fetchAndRenderBooks)
+
   function fetchAndRenderBooks(e){
     e.preventDefault()
-    console.log("Form submitted!!")
     const bookAdapter = new GoogleBooks($input.val())
-    // const newSpotifyAdapter = new Spotify()
-    bookAdapter.get()
-      .then(renderBooks)
-      // .then(function(book){
-      //   return book.volumeInfo
-      // })
-      // .then(function(volumeInfo){
-      //   return volumeInfo.title
-      // })
-      // .then(function(title){
-      //   debugger
-      // })
-      // .catch(function(){
-      //   console.log("Something went wrong!")
-      // })
+    bookAdapter.get().then(renderBooks)
+    $input.val('')
   }
 
-  // function simple(){
-  //   debugger
-  // }
-
   function renderBooks (data, status, request){
-    // takes in some data and renders out a list of books
-    console.log('response received!')
-    console.log(data)
     const bookListItems = data.items.map(function(book){
       return `<li>${book.volumeInfo.title}</li>`
     })
-    //
-    // url = `api.spotify.com/search?q=${book.volumeInfo.title}`
-    // $.ajax({
-    //   url: url,
-    //   success: function(data){
-    //
-    //   }
-    // })
-
     $bookList.html(bookListItems.join(''))
-    return data.items[0]
   }
-
-  // on submit, fires a function
-  $form.on('submit', fetchAndRenderBooks)
 })
--- src/adapters/googleBooks.js
+++ src/adapters/googleBooks.js
@@ -1,31 +1,13 @@
 class GoogleBooks {
   constructor(query){
     this.query = query
-    this.url = 'https://www.googleapis.com/books/v1/volumes'
+    this.baseUrl = 'https://www.googleapis.com/books/v1/volumes'
   }
 
   get(){
-    const url = `${this.url}?q=${this.query}`
-    console.log('Firing ajax request for ' + this.query)
+    const url = `${this.baseUrl}?q=${this.query}`
     return $.ajax({
       url: url
     })
   }
-
-  // post(data){
-  //   return $.ajax({
-  //     method: 'POST',
-  //     data: data,
-  //     url: url
-  //   })
-  // }
-
 }
-
-// class Spotify {
-//
-//   get(books){
-//     const query = books.items[0].volumeInfo.title
-//     return $.ajax("SPOTIFY/URL" + "?q=" + query)
-//   }
-// }
